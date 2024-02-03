3dbbbeb732ea8de40a4773ee3071193f67c48fdb
kanogame <wasdqert1222@gmail.com>
2024-02-03 16:44:09 +0700

    new inp

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -5,9 +5,12 @@ enum {
 };
 
 MainWindow::MainWindow(void) : BWindow(BRect(100,100,500,400),"Main Window",B_TITLED_WINDOW, B_ASYNCHRONOUS_CONTROLS) {
-	taskName = new BTextControl(BRect(5, 5, 300, 10), "box", "name", "put you input here", NULL, B_FOLLOW_LEFT | B_FOLLOW_RIGHT);
+	taskName = new BTextControl(BRect(5, 5, 300, 30), "name", NULL, "put you input here", NULL, B_FOLLOW_LEFT | B_FOLLOW_RIGHT);
 	AddChild(taskName);
-	BButton *button = new BButton(BRect(5, 500, 100, 20), "button", "enter", new BMessage(M_BUTTON_ENTER));
+	taskDesc = new BTextView(BRect(5, 40, 300, 100), "desc" ,BRect(0, 0, 300, 100), NULL);
+	AddChild(taskDesc);
+	taskDesc->SetText("put shit here");
+	BButton *button = new BButton(BRect(5, 200, 100, 20), "button", "enter", new BMessage(M_BUTTON_ENTER));
 	AddChild(button);
 }
 
